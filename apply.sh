7dba6b55d2a4474c6e0671f7c8e199940fff8f96
tomas f <46322567+TomFern@users.noreply.github.com>
2019-11-10 13:37:41 -0300

    streamline

--- apply.sh
+++ apply.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 # apply a kubernetes manifest
 
+set -e
+
 export deployment=$1
 export replicas=$2
 export img=$3
@@ -9,6 +11,4 @@ manifest=$(mktemp)
 cat manifests/deployment.yml | envsubst | tee $manifest
 kubectl apply -f $manifest
 kubectl rollout status -f $manifest
-rollout=$?
 rm -f $manifest
-exit $?
