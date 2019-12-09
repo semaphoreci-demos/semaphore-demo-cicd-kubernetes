bc5d17e883d9eaf422be5d54ee63a88ee6e8add6
Tommy F <46322567+TomFern@users.noreply.github.com>
2019-12-09 17:25:30 -0300

    Add rollback pipelines (#11)
    Add automated rollback for AWS, DO and GCP when functional tests on canary fail.

--- apply.sh
+++ apply.sh
@@ -18,5 +18,5 @@ export img=$4
 apply_tmp=$(mktemp)
 cat $manifest | envsubst | tee $apply_tmp
 kubectl apply -f $apply_tmp
-kubectl rollout status -f $apply_tmp
+kubectl rollout status -f $apply_tmp --timeout=120s
 rm -f $apply_tmp
