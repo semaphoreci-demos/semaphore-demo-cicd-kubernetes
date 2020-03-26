9428989bd3f98393288b40dd9aa8e0629957ae4f
Tommy F <46322567+TomFern@users.noreply.github.com>
2020-03-26 19:55:59 -0300

    Bump versions and improve docker image
    - Bump version of Node to 12.16.1 (LTS) and all the modules
    - Run Node inside the container as non-root
    - Stop using npm start as CMD as it intercepts signals
    - Add a migration service to docker compose to run migrations automatically.
    - Configure connection pool to sequelize for better performance
    - Add failure handler to migration script.

--- src/migrate.js
+++ src/migrate.js
@@ -1,3 +1,11 @@
 var db = require('./database.js');
-db.sequelize.sync();
+
+db.sequelize.authenticate()
+    .then(() => {
+        db.sequelize.sync()
+            // .then(console.log('Migration complete'))
+            .catch(console.error);
+    })
+    .catch(console.error);
+
 
